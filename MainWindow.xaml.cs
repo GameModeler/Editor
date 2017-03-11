bc57b2ce5ec41fa4fb86b95c6d1882ec490b52fe
Ronan Henry <Ronan Henry>
2017-03-11 17:34:15 +0100

    Add ViewModel and command bindings of main menu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using Editor.ViewModels;
 
 namespace Editor
 {
@@ -7,9 +11,140 @@ namespace Editor
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Attributes
+        #endregion
+
+        #region Properties
+
+        public EditorViewModel EditorViewModel { get; set; }
+
+        #endregion
+
+        #region Constructors
+
         public MainWindow()
         {
+            // Initialization
             InitializeComponent();
+            EditorViewModel = new EditorViewModel();
+            DataContext = EditorViewModel;
+
+            // Command Bindings
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.New, NewCommandExecuted));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Open, OpenCommandExecuted));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Save, SaveCommandExecuted, SaveCommandCanExecute));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, CloseCommandExecuted));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Find, BrowseCommandExecuted, BrowseCommandCanExecute));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Delete, DeleteCommandExecuted, DeleteCommandCanExecute));
+
+            // Event handlers
+            Closing += MainWindow_Closing;
         }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Command invoked to create a new map.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Command invoked to open a previously saved map.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks if the currently opened map can be saved during the save command invoke. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = false;
+        }
+
+        /// <summary>
+        /// Command invoked to save the current map.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Command invoked to quit the editor.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Checks if new assets can be added.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BrowseCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = false;
+        }
+
+        /// <summary>
+        /// Command invoked to browse for assets to load into the editor.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BrowseCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks if the assets can be deleted from the editor.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = false;
+        }
+
+        /// <summary>
+        /// Command invoked to delete assets from the editor.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Performs checks to avoid closing the editor without saving changes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            MessageBox.Show("Bye!");
+        }
+
+        #endregion
     }
 }
--- Views/PropertiesWindow.xaml.cs
+++ Views/PropertiesWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Editor.Views
 {
@@ -19,14 +7,22 @@ namespace Editor.Views
     /// </summary>
     public partial class PropertiesWindow : Window
     {
+        #region Constructors
+
         public PropertiesWindow()
         {
             InitializeComponent();
         }
 
+        #endregion
+
+        #region Methods
+
         private void PropertiesBtn_Clicked(object sender, RoutedEventArgs e)
         {
             Close();
         }
+
+        #endregion
     }
 }
